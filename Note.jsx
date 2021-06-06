cf302c09cae3245f0c0e2590510bc3507df99a95
Anushree Mathur <72229125+AnushreeMathur@users.noreply.github.com>
2021-06-06 20:56:37 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>JAVASCRIPT AND BOOTSTRAP</h1>
+      <p>
+        This was an amazing bootcamp taken up by Shaurya Sinha We covered
+        everything from Scratch including Javascript React.js, HTML.
+      </p>
+    </div>
+  );
+}
+export default Note;
---
+++ Note1.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note1() {
+  return (
+    <div className="note">
+      <h1>PYTHON AND MACHINE LEARNING</h1>
+      <p>
+        This was an amazing bootcamp taken up by Shaurya Sinha We covered
+        everything from Scratch including PYTHON, MACHINE LEARNING,CONCEPTS OF
+        LIBRARIES.
+      </p>
+    </div>
+  );
+}
+export default Note1;
